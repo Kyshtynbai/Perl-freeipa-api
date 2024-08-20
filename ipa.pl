f15be0e7e9bf1dfcf47c7be54252b663e8006f53
Ivan <imartynow@gmail.com>
2024-08-20 17:44:49 +0300

    Missing keys, user_hash forming functions added

--- ipa.pl
+++ ipa.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use Data::Dumper;
 
 my $input_users = shift @ARGV;
 
@@ -9,13 +10,33 @@ sub get_user_array(){
 	my $input_users = shift;
 	my $responce = `ipa user-show $input_users --all 2>&1`;
 	my @result;
-	print $responce;
+	my %hash;
 	if ($responce !~ /ERROR/g) {
-		return @result = split /\n/, $responce;
+		@result = split /\n/, $responce;
+		for (@result) {
+				s/^\s+//;
+				my ($key, $value) = split /:/;
+				$value =~ s/^\s+//;
+				$hash{$key} = $value;
+		}
+		return %hash;
 	} else {
-			return @result;
+			return %hash;
 	}
 }
 
-my @test = &get_user_array($input_users);
-print "$test[1], $test[2] \n" if @test;
+sub user_hash_missig_keys() {
+	my $h = shift;
+	$$h{"Pager Number"} = "N/A" if !$$h{"Pager Number"};
+	$$h{"Org. Unit"} = "N/A" if !$$h{"Org. UniPagert"};
+}
+
+my %user_hash = &get_user_array($input_users);
+if (%user_hash) {
+	&user_hash_missig_keys(\%user_hash);
+	for (sort keys %user_hash) {
+		print "$_ ===> $user_hash{$_}\n";
+		}
+	} else {
+		print "User $input_users not found\n";
+	}
